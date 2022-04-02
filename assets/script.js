3c4d43e2e30c8b72199a288bb5387a8a07160d8f
Novia Phan <noviaphan@gmail.com>
2022-04-02 19:35:17 -0400

    correctAnwser: changed the anwsers from numbers to the actual names of the anwser

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,62 @@
 
 
-
-// displaying quiz questions, using object literals to show questions and an array the questions that make our quiz. Arrays will make questions easy to iterate over
-
 var quizQuestions = [
-    // {
-    //     question: "No-Face spirit from 'Spirited Away' is based on Japanese Mythoology",
-    //     anwsers: ["True", "False"],
+   
 
-    //     correctAnwser: "1"
-    // },
+    
     {
         question: "Which Ghibhli character can tranform into a dragon?",
         anwsers: ["Howl","Haru","Haku", "Hatsumaru"],
 
-        correctAnwser: "2"
+        correctAnwser: "Haku"
     },
     {
         question: "What is the name of the fiery compainion in 'Howl's Moving Castle'?",
         anwsers: ["Heatfire","Sunray","Calipico","Calcifer"],
 
-        correctAnwser: "3"
+        correctAnwser: "Calicfer"
     },
     {
         question: "What is the nickname Sophie gives the cursed screcrow who follows her in 'Howl's Moving Castle'?",
         anwsers: ["Turnip Head","Gentleman","ScaryCrow","Happy Hat"],
 
-        correctAnwser: "0"
+        correctAnwser: "Turnip Head"
     },
     {
         question: "What are the names of the orphans that struggle in war-torn Japan in 'Grave of the Fireflies'?",
         anwsers: ["Haku & Sophie", "Totoro & Kiki","Rin & Rina","Seita & Setsuko"], 
 
-        correctAnwser: "3"
+        correctAnwser: "Seita & Setsuko"
     },
     {
         question: "What is the name of Kiki's kitty in 'Kiki's Delivery Service'?",
         anwsers: ["Luna","Jiji","Kiwi","Night"],
     
-        correctAnwser: "1"
+        correctAnwser: "Jiji"
     },
     {
         question: "What natural disaster occurs in 'Ponyo'?",
         anwsers: ["Earthquake","Hurricane","Tsunami","Tornado"],
     
-        correctAnwser: "2"
+        correctAnwser: "Tsunami"
     },
     {
         question: "What type of animals raise San in 'Princess Mononoke'?",
         anwsers: ["Tigers","Wolves","Deer","Bears"],
        
-        correctAnwser: "2"
+        correctAnwser: "Wolves"
     },
     {
         question: "What did Haku tell Chihiro to never forget in 'Spirited Away'?",
         anwsers: ["Her parents","Haku","Her Home","Her name"],
     
-        correctAnwser: "3"
+        correctAnwser: "Her Name"
     },
     {
         question: "What film features the Catbus?",
         anwsers: ["My Neighbor Totoro","Nausicaa of the Valley of the Wind","Ponyo","Spirited Away"],
     
-        correctAnwser: "0"
+        correctAnwser: "My Neighnor Totoro"
     },
 
 ];
@@ -87,6 +81,8 @@ var Time = document.querySelector('.Time')
 var question = document.querySelector('#question')
 var currentQuestion = 0;
 var acceptingQuestions = true;
+var choiceContainer = document.querySelector('#choiceContainer')
+
 
 var clockid=null
 
@@ -99,6 +95,43 @@ choiceOne.textContent=quizQuestions[currentQuestion].anwsers[0]
 choiceTwo.textContent=quizQuestions[currentQuestion].anwsers[1]
 choiceThree.textContent=quizQuestions[currentQuestion].anwsers[2]
 choiceFour.textContent=quizQuestions[currentQuestion].anwsers[3]
+
+choiceContainer.addEventListener('click', function(event){
+console.log(event.target.textContent)
+
+var userChoice = event.target.textContent
+var currentQuestionAnwser = quizQuestions[currentQuestion].correctAnwser
+console.log(userChoice)
+console.log(currentQuestionAnwser)
+
+// We need to remove the current question 
+// ...
+
+// We need to show the next question
+// ...
+
+// Increment the curentQuestion variable. 
+// ...
+
+   // User Pick      // actual anwer is
+if (userChoice === currentQuestionAnwser) {
+        alert("YOU GUESSED IT!")
+// We need to {add points} 
+
+
+} else {
+    // Loose points
+    alert("You SUCK!");
+}
+
+
+})
+
+
+
+
+
+
 
 
 
